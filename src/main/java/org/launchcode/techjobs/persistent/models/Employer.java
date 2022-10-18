89c8de155a610a7a8b7dc5e9afc01112178598ca
diegocolonia <diegocolonia98@yahoo.com>
2022-10-18 19:30:40 -0400

    commit

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -5,12 +5,14 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 @Entity
 public class Employer extends AbstractEntity {
 @NotEmpty
 @Size(min = 3,max = 50)
 @Length(min = 3,max = 50)
+@NotNull
 private String location;
 
     public Employer(String location) {
